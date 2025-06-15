8b602722f295a7210684e35bf7d4dc3c2b917d09
RAFIQ-ABDUL <rafiqabdulmanan@gmail.com>
2025-06-15 22:21:18 +0500

    initial commit

---
+++ app.py
@@ -0,0 +1,32 @@
+# app.py
+
+from flask import Flask, request, jsonify, render_template
+import pickle
+import numpy as np
+import pandas as pd
+
+# Load the trained model
+model_path = 'model31.pkl'
+with open(model_path, 'rb') as file:
+    model = pickle.load(file)
+
+app = Flask(__name__)
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    # Extract data from form
+    int_features = [float(x) for x in request.form.values()]  # Convert to float instead of int
+    final_features = np.array(int_features).reshape(1, -1)
+    # Make prediction
+    prediction = model.predict(final_features)
+    output = prediction[0]
+
+    return render_template('index.html', prediction_text='Prediction: {}'.format(output))
+
+
+if __name__ == "__main__":
+    app.run(debug=True,use_reloader=False)
